96de94c157f44fa4ab6a8503deb80bf41cc96aba
Himanshu Sahni <sahnihimanshu97@gmail.com>
2018-07-31 21:56:25 +0530

    Making UI changes
    Signed-off-by: Himanshu Sahni <sahnihimanshu97@gmail.com>

--- Attendance/app/src/main/java/com/online/attendencehelper/Activities/MainActivity.kt
+++ Attendance/app/src/main/java/com/online/attendencehelper/Activities/MainActivity.kt
@@ -6,15 +6,27 @@ import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
 import com.online.attendencehelper.R
+import com.online.attendencehelper.adapters.ShowSubjectRecyclerAdapter
+import com.online.attendencehelper.db.tables.SubjectTable
+import com.online.attendencehelper.db.tables.SubjectTableHelper
+import com.online.attendencehelper.models.Subject
 import kotlinx.android.synthetic.main.activity_main.*
+//import kotlinx.android.synthetic.main.activity_show_subject.*
 import kotlinx.android.synthetic.main.app_bar_nav.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var actIntent : Intent
+
+
+    var subjectList = ArrayList<Subject>()
+    lateinit var ShowSubjectAdapter : ShowSubjectRecyclerAdapter
+    lateinit var subjectObject: Subject
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,7 +39,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        showSubject()
+
+        btnFabAddSub.setOnClickListener{
+
+            actIntent = Intent(this, AddSubject::class.java)
+            startActivity(actIntent)
+        }
     }
+
 
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
@@ -72,11 +93,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 startActivity(actIntent)
 
             }
-            R.id.nav_show_subject-> {
-                actIntent = Intent(this, ShowSubject::class.java)
-                startActivity(actIntent)
-
-            }
             R.id.nav_report -> {
                 actIntent = Intent(this, ShowAttendanceReport::class.java)
                 startActivity(actIntent)
@@ -87,5 +103,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun showSubject() {
+        val db = SubjectTableHelper(this).readableDatabase
+        val subject = SubjectTable.getAllSubject(db)
+
+        for (i in subject){
+
+            subjectList.add(Subject(i.subjectid,i.subjectname,i.year,i.department,i.totalrollnos))
+        }
+        rvShowSubject.layoutManager = LinearLayoutManager(this)
+        ShowSubjectAdapter = ShowSubjectRecyclerAdapter(subjectList)
+        rvShowSubject.adapter = ShowSubjectAdapter
+
+
     }
 }
