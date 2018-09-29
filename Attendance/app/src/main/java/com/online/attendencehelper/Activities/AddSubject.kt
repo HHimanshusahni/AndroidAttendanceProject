6dfee3d634a80d198a2ca761d45644be52d4076b
Himanshu Sahni <sahnihimanshu97@gmail.com>
2018-09-29 13:57:02 +0530

    Add Subject Layout Improved with fab and dynamic schedule elments
    Signed-off-by: Himanshu Sahni <sahnihimanshu97@gmail.com>

--- Attendance/app/src/main/java/com/online/attendencehelper/Activities/AddSubject.kt
+++ Attendance/app/src/main/java/com/online/attendencehelper/Activities/AddSubject.kt
@@ -1,63 +1,65 @@
-package com.online.attendencehelper.Activities
+    package com.online.attendencehelper.Activities
 
 
-import android.content.Intent
-import android.graphics.Color
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
-import android.widget.*
-import com.online.attendencehelper.R
-import com.online.attendencehelper.datetime.DateTime
-import com.online.attendencehelper.datetime.TimePickerFragment
-import com.online.attendencehelper.db.tables.StudentTable
-import com.online.attendencehelper.db.tables.SubjectTable
-import com.online.attendencehelper.db.tables.TableHelper
-import com.online.attendencehelper.models.Student
-import com.online.attendencehelper.models.Subject
+    import android.content.Context
+    import android.content.Intent
+    import android.support.v7.app.AppCompatActivity
+    import android.os.Bundle
+    import android.view.View
+    import android.widget.*
+    import com.online.attendencehelper.R
+    import com.online.attendencehelper.datetime.DateTime
+    import com.online.attendencehelper.datetime.TimePickerFragment
+    import com.online.attendencehelper.db.tables.StudentTable
+    import com.online.attendencehelper.db.tables.SubjectTable
+    import com.online.attendencehelper.db.tables.TableHelper
+    import com.online.attendencehelper.models.Student
+    import com.online.attendencehelper.models.Subject
 
-import kotlinx.android.synthetic.main.activity_add_subject.*
-import kotlinx.android.synthetic.main.activity_add_subject.view.*
-import android.widget.TextView
-
-
-
-class AddSubject : AppCompatActivity() {
+    import kotlinx.android.synthetic.main.activity_add_subject.*
+    import android.view.LayoutInflater
+    import kotlinx.android.synthetic.main.schedule_field.view.*
 
 
-    lateinit  var actIntent: Intent
-    lateinit var linearLayoutNewView :LinearLayout
-    lateinit var btn :Button;
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_subject)
-        var subjectid:Int? =getIntent().getIntExtra("subject_id",-1)
-        Log.d("SUBB","$subjectid")
-        if(subjectid!=-1&&subjectid!=null) {
+    class AddSubject : AppCompatActivity() {
 
-            editSubject(subjectid!!)
-        }else{
-            subjectid =null
-        }
-        val datetime = DateTime()
-        datetime.setTime(btnTime)
 
-        btnTime.setOnClickListener {
-            // Initialize a new TimePickerFragment
-            val newFragment = TimePickerFragment(btnTime)
-            // Show the time picker dialog
-            newFragment.show(fragmentManager, "Time Picker")
-        }
+        lateinit  var actIntent: Intent;
+        lateinit var  parentLinearLayout:LinearLayout
 
-        spinnerDays()
 
-        // store in tables
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_add_subject)
 
-        btnAddNew.setOnClickListener{
-           addDynamicLayoutSchedule()
+            parentLinearLayout = LinearLayout(this)
+            parentLinearLayout= findViewById(R.id.parentLinearSchdule)
+
+            var subjectid:Int? =getIntent().getIntExtra("subject_id",-1)
+            if(subjectid!=-1&&subjectid!=null) {
+
+                editSubject(subjectid!!)
+            }else{
+                subjectid =null
+            }
+
+
+            onAddField(findViewById(R.id.btnAddNew))// adding the first schdule detail
+
+            btnSubmitSubject.setOnClickListener{
+
+                submitSubject(subjectid)
+
+
+            }
+
 
         }
-        btnSubmitSubject.setOnClickListener{
+
+
+
+
+        fun submitSubject(subjectid: Int?){
 
             if(etSubjectName.text.toString().length==0||
                     etDepartment.text.toString().length==0||
@@ -88,76 +90,52 @@ class AddSubject : AppCompatActivity() {
                     StudentTable.addStudents(db,studentList)
                 }
                 // Modify Code  for case Rollno's entered as different range of roll no's required and skipped  rollno's
-
                 actIntent = Intent(this, MainActivity::class.java)
                 startActivity(actIntent)
                 Toast.makeText(this, "Subject Added", Toast.LENGTH_SHORT).show()
+
             }
+        }
+
+
+
+         fun editSubject(subjectId:Int){
+            val db = TableHelper(this).readableDatabase
+            val subject = SubjectTable.getSubjectFromId(db, subjectId)
+            etSubjectName.setText(subject.subjectname)
+            etDepartment.setText(subject.department)
+            etRollNo.setText(""+subject.totalrollnos)
+            etYear.setText(""+subject.year)
 
         }
 
 
-    }
+
+
+        fun onAddField( view : View){
+
+            val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val rowView = inflater.inflate(R.layout.schedule_field, null)
+            parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount())
+
+            val datetime = DateTime()
+            datetime.setTime(parentLinearLayout.getChildAt(parentLinearLayout.childCount-1).btnTime)
+
+
+        }
 
 
 
-    private fun spinnerDays(){
-        //https://developer.android.com/guide/topics/ui/controls/spinner
-        val spinner : Spinner = findViewById(R.id.spinnerDays)
-        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
-                this,
-                R.array.days_array,
-                android.R.layout.simple_spinner_item
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.setAdapter(adapter)
+        fun onDelete(view :View){
+            parentLinearLayout.removeView(view.getParent()as View)
 
-    }
-    private fun editSubject(subjectId:Int){
-        val db = TableHelper(this).readableDatabase
-        val subject = SubjectTable.getSubjectFromId(db, subjectId)
-        etSubjectName.setText(subject.subjectname)
-        etDepartment.setText(subject.department)
-        etRollNo.setText(""+subject.totalrollnos)
-        etYear.setText(""+subject.year)
-
-    }
-    private fun addDynamicLayoutSchedule(){
-        var linearLayout = LinearLayout(this)
-        linearLayoutNewView = findViewById(R.id.parentLinearSchdule)
+        }
 
 
-        var buttontime = Button(this)
-        val datatime = DateTime()
-        buttontime.setText(datatime.getTime())
-        buttontime.setOnClickListener{
-            val newFragment = TimePickerFragment(buttontime)
-            // Show the time picker dialog
+        fun showTimePicker(view:View){
+
+            val newFragment = TimePickerFragment(view as TextView)
             newFragment.show(fragmentManager, "Time Picker")
         }
 
-
-
-        val spinner  = Spinner(this)
-        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
-                this,
-                R.array.days_array,
-                android.R.layout.simple_spinner_item
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.setAdapter(adapter)
-
-
-
-        var deleteButton = Button(this)
-        deleteButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_delete_black_24dp, 0, 0, 0);
-        deleteButton.setBackgroundColor(Color.TRANSPARENT)
-        linearLayout.setOrientation(LinearLayout.HORIZONTAL)
-        linearLayout.addView(buttontime)
-        linearLayout.addView(spinner)
-        linearLayout.addView(deleteButton)
-        linearLayoutNewView.addView(linearLayout)
-
     }
-
-}
